f60c9ca04b79715349a045f4f2c2b645eedf52be
Kevin <kevin.chevalley@gmail.com>
2020-11-16 16:01:02 +0100

    Ajout du template de page

--- single.php
+++ single.php
@@ -1,23 +1,24 @@
 <?php get_header(); ?>
 <div class='loop'>
-<div class="main single">
-<?php if (have_posts()) : ?>
-<?php while (have_posts()) : the_post(); ?>
-<div class="post">
-<h1 class="post-title"><?php the_title(); ?></h1>
-<p class="post-info">
-Posté le <?php the_date(); ?> dans <?php the_category(', '); ?> par <?php the_author(); ?>.
-</p>
-<div class="post-content">
-<?php the_content(); ?>
+    <div class="main single">
+        <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+        <div class="post">
+            <h1 class="post-title"><?php the_title(); ?></h1>
+            <p class="post-info">
+                Posté le <?php the_date(); ?> dans <?php the_category(', '); ?> par <?php the_author(); ?>.
+            </p>
+            <div class="post-content">
+                <?php the_content(); ?>
+            </div>
+            <div class="post-comments">
+                <?php comments_template(); ?>
+            </div>
+        </div>
+    </div>
+    <?php endwhile; ?>
+    <?php endif; ?>
 </div>
-<div class="post-comments">
-<?php comments_template(); ?>
-</div>
-</div>
-<?php endwhile; ?>
-<?php endif; ?>
-</div>
-</div>
+
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
